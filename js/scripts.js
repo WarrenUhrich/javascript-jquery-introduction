609a0d2f8d235e9e3a694398d998dc6295420e8e
Warren Uhrich <hello@warren.codes>
2020-04-02 18:59:29 -0600

    Add jQuery element selection.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * Using jQuery!
+ * NOTE: The "$" is a regular character. It can be used in a variable
+ *       or function name the same as any regular letter. The jQuery
+ *       developers programmed jQuery all under one function called
+ *       jQuery(). Because it is annoying to type jQuery all the time
+ *       they decided to do "$ = jQuery" so you could use the "$" by
+ *       itself as a shorthand!
+ */
+
+const myHeading = $( 'h1' ); // Grabbing an element.
+console.log( myHeading );
